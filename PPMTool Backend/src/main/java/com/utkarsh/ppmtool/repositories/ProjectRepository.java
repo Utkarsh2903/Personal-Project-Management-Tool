058d84ff70002ddaeae3c88b9f5ac8e3a897f071
Utkarsh2903 <uagrawal29.ua@gmail.com>
2022-03-19 23:50:04 +0530

    Define Project Object and Add Project Repositiry

---
+++ PPMTool Backend/src/main/java/com/utkarsh/ppmtool/repositories/ProjectRepository.java
@@ -0,0 +1,14 @@
+package com.utkarsh.ppmtool.repositories;
+
+import com.utkarsh.ppmtool.domain.Project;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+
+    @Override
+    default Iterable<Project> findAllById(Iterable<Long> longs) {
+        return null;
+    }
+}
