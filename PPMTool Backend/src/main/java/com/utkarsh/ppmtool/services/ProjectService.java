9cea9daf7fccb383a4468eff9a2376281cdaeee7
Utkarsh2903 <uagrawal29.ua@gmail.com>
2022-03-21 23:58:42 +0530

    Add custom exception handler for project identifier

--- PPMTool Backend/src/main/java/com/utkarsh/ppmtool/services/ProjectService.java
+++ PPMTool Backend/src/main/java/com/utkarsh/ppmtool/services/ProjectService.java
@@ -1,6 +1,7 @@
 package com.utkarsh.ppmtool.services;
 
 import com.utkarsh.ppmtool.domain.Project;
+import com.utkarsh.ppmtool.exceptions.ProjectIdException;
 import com.utkarsh.ppmtool.repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,7 +13,11 @@ public class ProjectService {
     private ProjectRepository projectRepository;
 
     public Project saveOrUpdateProject(Project project) {
-
-        return projectRepository.save(project);
+        try {
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
+        } catch (Exception e) {
+            throw new ProjectIdException("Project ID '" + project.getProjectIdentifier().toUpperCase() + "' already exists");
+        }
     }
 }
